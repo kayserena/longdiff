36c5a5eb81a319d830ce75ee2583818662398c1c
Kayla Esser <kaylaesser@gmail.com>
2023-11-17 15:56:39 -0500

    Initial commit

---
+++ R/longdiff.R
@@ -0,0 +1,7 @@
+#' @title differences in longitudinal data function
+#' @description a function that returns the rows of data in which a condition is met that indicates a difference in a variable across two timepoints, for use with longitudinal data
+#' @param x numeric vector
+#' @return y
+#' @author Kayla Esser
+#' @examples
+#' @export
