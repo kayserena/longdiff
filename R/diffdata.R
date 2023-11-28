cc013e0c56582450396b2a1d80f952cb6e2716e1
Kayla Esser <kaylaesser@gmail.com>
2023-11-27 22:55:52 -0500

    version 1 of longdiff with greater than operator

---
+++ R/diffdata.R
@@ -0,0 +1,13 @@
+#' Diff Data
+#'
+#' Test data for testing longdiff function
+#'
+#' @docType data
+#' @format a data frame with 30 rows and 3 variables
+#' \describe{
+#' \item{record_id}{the ID variable of each subject}
+#' \item{timepoint}{the timepoint of each data collection event, such as year}
+#' \item{test_score}{the test score, a continuous variable}
+#' }
+
+"diffdata"
--- R/longdiff.R
+++ R/longdiff.R
@@ -1,7 +1,30 @@
 #' @title differences in longitudinal data function
-#' @description a function that returns the rows of data in which a condition is met that indicates a difference in a variable across two timepoints, for use with longitudinal data
-#' @param x numeric vector
-#' @return y
+#' @description a function that returns the IDs of data in which a condition is met that indicates a positive difference in a variable across two timepoints, for use with longitudinal data in long format
+#' @param data the name of your dataset
+#' @param ID the variable name corresponding to individual record or subject IDs
+#' @param timevar the variable corresponding to timepoint (year, month, etc) of your data
+#' @param var the variable you are interested in looking at differences within
+#' @param timeparam the value of the timepoint you are interested in comparing to baseline (such as the year)
+#' @param cutoff the cutoff value that you would like to set for your variable of interest
+#' @return a vector listing all subject IDs that meet the cutoff criteria for your specified variable
 #' @author Kayla Esser
 #' @examples
+#' library(dplyr)
+#' list <- longdiff(diffdata, record_id, timepoint, test_score, 2002, 0)
+#' list
 #' @export
+
+longdiff <- function(data, ID, timevar, var, timeparam, cutoff) {
+  names(data)[names(data) == deparse(substitute(ID))] <- "ID"
+  names(data)[names(data) == deparse(substitute(timevar))] <- "timevar"
+  names(data)[names(data) == deparse(substitute(var))] <- "var"
+
+  alldiff <- data |>
+    dplyr::select(ID, timevar, var) |>
+    dplyr::arrange(ID, timevar) |>
+    dplyr::group_by(ID) |>
+    dplyr::filter(timevar == timeparam & (var - var[1L]) > cutoff) |>
+    dplyr::select(ID) |>
+    dplyr::pull(ID)
+  return(alldiff)
+}
